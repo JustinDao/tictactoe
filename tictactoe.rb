acb144de9f25f293bd1e068f170360689b6ca3a6
Justin Dao <tjd5at@virginia.edu>
2015-02-16 17:29:31 -0500

    Updates to include cats games

--- tictactoe.rb
+++ tictactoe.rb
@@ -16,11 +16,9 @@ def tictactoe
       permutations.each do |third|
         board = [first, second, third]
         # Check to see if it is valid board and a valid winning board
-        if valid_board?(board) && valid_three_in_a_row?(board)
+        if valid_three_in_a_row?(board) || valid_cats_game?(board)
           # Add to valid games
           valid_games.push(board)
-          # print_board(board)
-          # puts
         end
       end
     end
@@ -29,12 +27,77 @@ def tictactoe
   puts "Number of possbile endings: #{valid_games.count}"
 end
 
-# Checks to see if board is a playable board.
+# Checks to see if board is a playable board
+# returns if a 3x3 board has a single three in a row
+# and checks to see if it is a valid, playable board
 # If x_first_only is set to false, then we also look at boards where O goes first
 # This should just double the output number if set to false
-def valid_board?(board, x_first_only=true)
+def valid_three_in_a_row?(board, x_first_only=true)
+  # Must be square input
+  board.each do |row|
+    if (board.length != 3 || row.length != 3)
+      raise TypeError, "Input is not a 2D square (n x n) array"
+    end
+  end
 
-  # Get counts for each type of move
+  winning_set = get_winning_set(board)
+
+  # There can only be one three in a row
+  # Ex. X | | O
+  #     -------
+  #     X | | O
+  #     -------
+  #     X | | O
+  # is not valid
+  if winning_set.count == 1
+    # Check to see if it is a valid playable board
+    winner = winning_set.to_a.first
+
+    # Get counts for each player
+    x_count = count_board(board, "X")
+    o_count = count_board(board, "O")
+
+    o_first = false
+
+    # If O is allowed to go first, then number of possibilities increases
+    unless x_first_only
+      # If O goes first, this is a valid board structure
+      o_first = (x_count+1 == o_count && winner == "O") || (x_count == o_count && winner == "X")
+    end
+
+    # If X wins, it must take 1 more than O
+    # If O wins, there must be the same number of X's and O's
+    return  (
+              (
+                x_count == o_count && winner == "O"
+              ) ||
+              (
+                x_count == o_count+1 && winner == "X"
+              ) || 
+              o_first
+            )
+  end
+  
+  return false
+end
+
+def valid_cats_game?(board, x_first_only=true)
+  # Must be square input
+  board.each do |row|
+    if (board.length != 3 || row.length != 3)
+      raise TypeError, "Input is not a 2D square (n x n) array"
+    end
+  end
+
+  # Get the winner of the board
+  winner = get_winner(board)
+
+  # If there is a winner, it is not a cats game
+  if not winner == " "
+    return false
+  end
+
+  # Get counts for each player
   x_count = count_board(board, "X")
   o_count = count_board(board, "O")
 
@@ -43,90 +106,26 @@ def valid_board?(board, x_first_only=true)
   # If O is allowed to go first, then number of possibilities increases
   unless x_first_only
     # If O goes first, this is a valid board structure
-    o_first = (x_count+1 == o_count && get_winner(board) == "O") || (x_count == o_count && get_winner(board) == "X")
+    o_first = (x_count+1 == o_count)
   end
 
-  # If X wins, it must take 1 more than O
-  # If O wins, there must be the same number of X's and O's
-  return  (
-            (
-              x_count == o_count && get_winner(board) == "O"
-            ) ||
-            (
-              x_count == o_count+1 && get_winner(board) == "X"
-            ) || 
-            o_first
-          )
-
-end
-
-# returns if a 3x3 board has a single three in a row
-def valid_three_in_a_row?(board)
-  # Must be square input
-  board.each do |row|
-    if (board.length != 3 || row.length != 3)
-      raise TypeError, "Input is not a 2D square (n x n) array"
-    end
-  end
-
-  # Create set to hold winner
-  winner = get_winning_set(board)
-
-  # There can only be one three in a row
-  # Ex. X | | O
-  #     -------
-  #     X | | O
-  #     -------
-  #     X | | O
-  # is not valid
-  if winner.count == 1
-    return true
-  end
-  
-  return false
-end
-
-def get_winner(board)
-  # Must be square input
-  board.each do |row|
-    if (board.length != 3 || row.length != 3)
-      raise TypeError, "Input is not a 2D square (n x n) array"
-    end
-  end
-
-  winner = get_winning_set(board)
-
-  # There can only be one three in a row
-  # Ex. X | | O
-  #     -------
-  #     X | | O
-  #     -------
-  #     X | | O
-  # is not valid
-  if winner.count == 1
-    return winner.to_a.first
-  end
-  
-  return nil
+  # For cats game, board must not have any empty spaces
+  return  x_count + o_count == 9 &&
+          # Number of X's must be 1 more than O's
+          # Unless x_first_only is false
+          (x_count == o_count + 1 || o_first)
 end
 
 def get_winning_set(board)
-  # Must be square input
-  board.each do |row|
-    if (board.length != 3 || row.length != 3)
-      raise TypeError, "Input is not a 2D square (n x n) array"
-    end
-  end
-
   # Create set to hold winner
-  winner = Set.new
+  winning_set = Set.new
 
   # check rows
   (0..2).each do |row_index|
     if board[row_index][0] == board[row_index][1] &&
        board[row_index][0] == board[row_index][2]
       
-      winner.add(board[row_index][0])
+      winning_set.add(board[row_index][0])
     end
   end
 
@@ -135,7 +134,7 @@ def get_winning_set(board)
     if board[0][column_index] == board[1][column_index] &&
        board[0][column_index] == board[2][column_index]
       
-      winner.add(board[0][column_index])
+      winning_set.add(board[0][column_index])
     end
   end
 
@@ -144,21 +143,33 @@ def get_winning_set(board)
   if board[0][0] == board[1][1] &&
      board[0][0] == board[2][2]
 
-    winner.add(board[0][0])
+    winning_set.add(board[0][0])
   end
 
   # top right -> bottom left
   if board[0][2] == board[1][1] &&
      board[0][2] == board[2][0]
 
-    winner.add(board[0][2])
+    winning_set.add(board[0][2])
   end
 
   # If there is a three in a row of spaces, ignore
-  winner.delete(" ")
+  winning_set.delete(" ")
 
-  return winner
+  return winning_set
 end
+
+def get_winner(board)
+  winning_set = get_winning_set(board)
+
+  if winning_set.count == 1
+    return winning_set.to_a.first
+  elsif winning_set.count == 0
+    return " "
+  end
+
+  return nil
+end 
 
 # Counts the number of times a marker appears on the board
 def count_board(board, marker)
